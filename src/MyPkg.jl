e9418a0105dd6b8926530202e2dffb90b7d8dc53
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2024-06-04 14:02:13 +0200

    Some code

--- src/MyPkg.jl
+++ src/MyPkg.jl
@@ -2,4 +2,8 @@ module MyPkg
 
 # Write your package code here.
 
+function foo(x)
+  return "Hello $x"
+end
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "MyPkg.jl" begin
   # Write your tests here.
+  @test MyPkg.foo("world") == "Hello world"
 end
